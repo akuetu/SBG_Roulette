581a3bfcd1de928ca94a07d50605501de60fc3cb
Emanuel Mateus <emanuel.akuetu@gmail.com>
2021-12-27 12:56:12 +0000

    added  Even Bet class

---
+++ Roulette.Core/Rules/EvenBet.cs
@@ -0,0 +1,21 @@
+﻿using Roulette.Core.Interface;
+using Roulette.Core.Model;
+
+namespace Roulette.Core.Rules
+{
+    public class EvenBet : IBet
+    {
+        private readonly BetModel _betModel;
+
+        public EvenBet(BetModel betModel)
+        {
+            _betModel = betModel;
+        }
+
+        public bool CalculateBet()
+        {
+            return _betModel.WheelNumber % 2 == 0;
+        }
+
+    }
+}
