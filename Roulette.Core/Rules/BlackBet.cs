c9e48db131b7b6c7e03fe24ee39ce386f320dd7c
Emanuel Mateus <emanuel.akuetu@gmail.com>
2021-12-27 12:22:56 +0000

    added BlackBet class

---
+++ Roulette.Core/Rules/BlackBet.cs
@@ -0,0 +1,21 @@
+﻿using Roulette.Core.Base;
+using Roulette.Core.Interface;
+using Roulette.Core.Model;
+
+namespace Roulette.Core.Rules
+{
+    public class BlackBet : IBet
+    {
+        private readonly BetModel _betModel;
+
+        public BlackBet(BetModel betModel)
+        {
+            _betModel = betModel;
+        }
+        public bool CalculateBet()
+        {
+            var blackPieces = Common.BlackPieces();
+            return blackPieces.Contains(_betModel.WheelNumber);
+        }
+    }
+}
