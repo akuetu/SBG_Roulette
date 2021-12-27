4de29ef0716e87f0730b74c71397892940950b55
Emanuel Mateus <emanuel.akuetu@gmail.com>
2021-12-27 13:13:38 +0000

    clean Odd Bet

--- Roulette.Core/Rules/OddBet.cs
+++ Roulette.Core/Rules/OddBet.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Roulette.Core.Interface;
+﻿using Roulette.Core.Interface;
 using Roulette.Core.Model;
 
 namespace Roulette.Core.Rules
