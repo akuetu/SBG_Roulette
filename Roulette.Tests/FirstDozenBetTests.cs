8ad394eb2f1582cb450372f09d7dc22b61598d61
Emanuel Mateus <emanuel.akuetu@gmail.com>
2021-12-27 14:17:09 +0000

    added FirstDozen Bet Tests

---
+++ Roulette.Tests/FirstDozenBetTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Roulette.Core;
+using Roulette.Core.Base;
+using Roulette.Core.Model;
+using Xunit;
+
+namespace Roulette.Tests
+{
+    public class FirstDozenBetTests
+    {
+        private readonly RouletteBet _rouletteBet;
+        private readonly Board _board;
+        private const int Rows = 12;
+        private const int Cols = 3;
+
+        public FirstDozenBetTests()
+        {
+            _rouletteBet = new RouletteBet();
+            _board = new Board(Rows, Cols);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void CalculateBet_With_Data_Should_ReturnTrueWhenBetAndSpinAreCorrect(List<int> bet, int wheelNumber)
+        {
+            var betModel = new BetModel()
+            {
+                TypeOfBet = TypeOfBet.FirstDozen,
+                Bets = bet.ToArray(),
+                WheelNumber = wheelNumber,
+                Board = _board
+            };
+
+            var result = _rouletteBet.CalculateBet(betModel, new Account(500));
+
+            Assert.True(result.Status);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(DataWithInvalidValues))]
+        public void CalculateBet_With_Data_Should_ReturnFalseWhenBetAndSpinAreIncorrect(List<int> bet, int wheelNumber)
+        {
+            var betModel = new BetModel()
+            {
+                TypeOfBet = TypeOfBet.FirstDozen,
+                Bets = bet.ToArray(),
+                WheelNumber = wheelNumber,
+                Board = _board
+            };
+
+            var exception = Assert.Throws<ArgumentException>(() => _rouletteBet.CalculateBet(betModel, new Account(500)));
+
+            Assert.Equal("Invalid Argument.", exception.Message);
+        }
+
+        public static IEnumerable<object[]> Data()
+        {
+            yield return new object[] { new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, 10 };
+        }
+
+        public static IEnumerable<object[]> DataWithInvalidValues()
+        {
+            yield return new object[] { new List<int> { 1, 3, 7, 10, 13, 16, 19, 20, 25, 28, 31, 34 }, 10 };
+        }
+
+
+    }
+}
