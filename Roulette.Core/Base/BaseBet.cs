264cc11cd80247402d6e20f98a6779c243f1234a
Emanuel Mateus <emanuel.akuetu@gmail.com>
2021-12-27 12:21:45 +0000

    added BaseBet class

---
+++ Roulette.Core/Base/BaseBet.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Roulette.Core.Base
+{
+    public class BaseBet
+    {
+        public static bool IsCorrectBet(int wheelNumber, int[] bets)
+        {
+            return bets.Contains(wheelNumber);
+        }
+    }
+}
