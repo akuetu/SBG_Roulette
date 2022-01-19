4ad59c24d0f950255cf812952ccba8cfe42594b5
Emanuel Mateus <emanuel.akuetu@gmail.com>
2022-01-19 01:54:04 +0000

    added service folder and refactor service project

---
+++ Roulette.Core/Service/IRouletteService.cs
@@ -0,0 +1,10 @@
+﻿using Roulette.Core.Base;
+using Roulette.Core.Model;
+
+namespace Roulette.API.Services
+{
+    public interface IRouletteService
+    {
+        public BetResponse CalculateBet(TypeOfBet typeOfBet, int[] bet, Account account);
+    }
+}
